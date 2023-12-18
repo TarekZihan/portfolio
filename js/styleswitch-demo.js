b6fe8ea473b41231eb3bceb51a377ca84a048344
tareq <tareq@gmail.com>
2023-12-18 10:08:15 +0600

    portfolio

---
+++ js/styleswitch-demo.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(".switcher-control").on('click', function(){
+	  $('.switcher').toggleClass('toggled');
+	});
+
+	$(".styleswitch").on('click', function(){
+	  $(".styleswitch").removeClass('active');
+	  $(this).addClass('active');
+	});
+	
+});
